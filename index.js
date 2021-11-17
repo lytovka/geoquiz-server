a1d0fca385c44ec652a5d6d8feea0c8366635c92
Vlad <vladislav.brovikov@gmail.com>
2021-11-17 16:08:32 -0500

    set up a simple server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
 
 const Countries = require("./models/Country")
 
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect(
     "mongodb+srv://dbUser:a6qsxUg6akKH8XzE@cluster0.6ngci.mongodb.net/countries?retryWrites=true&w=majority",
@@ -13,9 +15,16 @@ mongoose.connect(
     }
 )
 
-app.get("/", async(req, res) => {
-    Countries.findOne({ "data.name": "Angola" }).lean().exec(function(err, docs) {
-        console.log(docs);
+app.get("/read/:country_key", async (req, res) => {
+    const country_key = req.body;
+    let query = { country_key: req.params.country_key }
+    console.log('requested to see: ', req.params.country_key)
+
+    Countries.findOne(query).lean().exec(function (err, docs) {
+        if (err) {
+            res.send(err)
+        }
+        res.send(docs)
     });
 })
 
