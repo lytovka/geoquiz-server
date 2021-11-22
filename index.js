1d20116c7bc9fe698dfcb19509d60aec5e5b3d18
Vlad <vladislav.brovikov@gmail.com>
2021-11-22 16:43:45 -0500

    added static links to images

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 const Countries = require('./models/Country');
+const PAGE_SIZE = 48;
 
 app.use(express.json());
 app.use(cors());
+app.use('/flags', express.static('images'))
 
 mongoose.connect(
   'mongodb+srv://dbUser:a6qsxUg6akKH8XzE@cluster0.6ngci.mongodb.net/countries?retryWrites=true&w=majority',
@@ -30,10 +33,32 @@ app.get('/read/:country_key', async (req, res) => {
     });
 });
 
+
 app.get('/countries', async (req, res) => {
   let query = {};
+  console.log('requests info about all countries');
+
+  Countries.find(query)
+    .select('country_key data.name data.flag')
+    .lean()
+    .exec(function (err, docs) {
+      if (err) {
+        res.send(err);
+      }
+      res.send(docs);
+    });
+});
+
+app.get('/countries/:page_number', async (req, res) => {
+  const limit = PAGE_SIZE;
+  const skip = parseInt(req.params.page_number) * PAGE_SIZE;
+  let query = {};
+  console.log('requested page number ', req.params.page_number);
+
   Countries.find(query)
     .select('country_key flag data.name')
+    .skip(skip)
+    .limit(limit)
     .lean()
     .exec(function (err, docs) {
       if (err) {
